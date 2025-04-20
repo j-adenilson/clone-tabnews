2e498c87004fe514d268eb0551c96fc1c0214c48
j-adenilson <j.adenilson@outlook.com.br>
2025-04-20 03:30:27 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>"Demore o tempo que for para decidir o que você quer da vida, e depois que decidir não recue ante nenhum pretexto, porque o mundo tentará te dissuadir."</h1>
+}
+
+export default Home;
