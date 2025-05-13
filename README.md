b201ad18569fbe844fbefa086aa3bc02d8fe1fb8
j-adenilson <j.adenilson@outlook.com.br>
2025-05-13 11:24:34 +0000

    adiciona scripts `lint:cheeck` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para treinar programação, desenvolvendo uma aplicação do começo ao fim
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>"Respondeu Jesus: "Eu sou o caminho, a verdade e a vida. Ninguém vem ao Pai, a não ser por mim."</h1>
+function Home() {
+  return (
+    <h1>
+      "Respondeu Jesus: "Eu sou o caminho, a verdade e a vida. Ninguém vem ao
+      Pai, a não ser por mim."
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console("teste2 para erro");
 }
 
 export default Home;
