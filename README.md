5d92bd8bc8c4fd43d6abf7115419ed9ad1aa4aad
j-adenilson <j.adenilson@outlook.com.br>
2026-03-10 22:33:43 -0300

    adiciona scripts `lint:check e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para estudos de programação, desenvolvendo uma aplicação de ponta a ponta.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Amo você Mary! passo o dia inteiro pensando nas nossas noites... 😮‍💨 😈 😶‍🌫️ </h1>
+function Home() {
+  return (
+    <h1>
+      Amo você Mary! passo o dia inteiro pensando nas nossas noites... 😮‍💨 😈
+      😶‍🌫️{" "}
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
